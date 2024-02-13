06462faaaa6656cb3f5be47d68402e6b369ee8c0
Anil Caliskan <anilclskn.1@gmail.com>
2024-02-13 20:38:45 +0300

    Initial Commit

---
+++ DatePlannerApp/DatePlannerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePlannerAppApp.swift
+//  DatePlannerApp
+//
+//  Created by Anil Caliskan on 13.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct DatePlannerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
